eb3dc3bc3b362102c03d64fc0c7919a9071a5442
vovabndr <vovabndr6661@icloud.com>
2016-11-15 19:09:37 +0200

    Initial Commit

---
+++ lab6(csfml)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab6(csfml)
+//
+//  Created by Владимир Бондарь on 11/15/16.
+//  Copyright © 2016 vbbv. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
