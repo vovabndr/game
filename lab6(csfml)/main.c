b679fb5b506f2ce0560841e7ade99a91ab6f6dcf
vovabndr <vovabndr6661@icloud.com>
2016-12-03 01:19:46 +0200

    FINAL COMMIT

--- lab6(csfml)/main.c
+++ lab6(csfml)/main.c
@@ -7,13 +7,10 @@
 //
 
 #include <SFML/Graphics.h>
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
 
 typedef struct
 {
@@ -21,26 +18,19 @@ typedef struct
     int y;
 } Point;
 
-
+Point* maxWay;
 int maxSum = -1;
 int map[10][10];
-Point* maxWay;
 int sizeOfWay;
-
-
+void findWay(int x, int y, int sum, Point* way, int counter);
 int rtrnX(int numb);
 int rtrnY(int numb);
-void Mdelay(unsigned int mseconds);
-
-void findWay(int x, int y, int sum, Point* way, int counter);
-
 void currentJSON();
-void writeMap();
-void readMap();
+void readJSON();
+void Mdelay(unsigned int mseconds);
 
 int main(int argc,char* argv[])
 {
-    
     sfVideoMode mode ={768,768,32};
     sfRenderWindow* window;
     
@@ -52,7 +42,6 @@ int main(int argc,char* argv[])
     sfTexture* goldIconTexture;
     sfTexture* doorTexture;
     sfTexture* stepsTexture;
-    
     
     sfSprite* blockSprite[47];
     sfSprite* grassSprite[100];
@@ -68,30 +57,25 @@ int main(int argc,char* argv[])
     sfVector2f goldScale;
     
     sfColor textColor;
-    
     sfEvent event;
-    
     sfFont* font;
-    
     sfText* text;
     sfText* goldText;
     sfText* goldIconText[20];
     sfText* fpsText;
-    
     sfIntRect heroRect;
-    
     sfClock *fpsClock;
+    
+    
     fpsClock = sfClock_create();
 
     
-    int i,j,k,count=0,goldSpriteCount=0,gold=0,currentGoldCount=0,s=0,t=0,check,forS;
+    int i,j,k,count=0,goldSpriteCount=0,gold=0,currentGoldCount=0,s=0,t=0,check,forS,argvCheck=-1,fps = 0;
     int stepMap[10][10];
+    double speed=1;
     char goldArr[4];
-    heroRect.width=52;
-    heroRect.height=76;
-    
+    Point* way = (Point*) malloc(sizeof(Point) * 100);
     srand((unsigned int)time(NULL));
-    
     
     char blockAddress[]="/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/Wall_DelbekeHannes copy.JPG";
     char grassAddress[]="/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/rsz_so4m8w0.png";
@@ -102,51 +86,50 @@ int main(int argc,char* argv[])
     char textAddress[]="/Library/Fonts/Arial Black.ttf";
     char goldIconAddress[]="/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/monetary_icon.png";
     char stepsAddress[]="/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/steps.png";
-    
-    if (!fpsClock)
+    char argvCheckLoad[]="loadto";
+    char argvChecksave[]="saveto";
+    char* argvText;
+
+    if(argv[1]!=NULL)
     {
-        return 1;
+        argvText=argv[1];
+        for (i=0; i<8;i++ )
+        {
+            if (argvText[i]!=argvCheckLoad[i])
+                break;
+                else
+                argvText=0;//argv[0]-loadto
+        }
+        for (i=0; i<8;i++ )
+        {
+            if (argvText[i]!=argvChecksave[i])
+                break;
+            else
+                argvCheck=1;//argv[1]-saveto
+        }
     }
     
+    if (!fpsClock)      return EXIT_FAILURE;
     window = sfRenderWindow_create(mode, "lab 6", sfResize | sfClose, NULL);
-    if (!window)
-        return 2;
-    
+    if (!window)        return EXIT_FAILURE;
     blockTexture = sfTexture_createFromFile(blockAddress, NULL);
-    if (!blockTexture)
-        return 3;
-    
+    if (!blockTexture)  return EXIT_FAILURE;
     grassTexture = sfTexture_createFromFile(grassAddress, NULL);
-    if (!grassTexture)
-        return 4;
-    
+    if (!grassTexture)  return EXIT_FAILURE;
     boxTexture = sfTexture_createFromFile(boxAddress, NULL);
-    if (!boxTexture)
-        return 5;
-    
+    if (!boxTexture)    return EXIT_FAILURE;
     heroTexture = sfTexture_createFromFile(heroAddress, NULL);
-    if (!heroTexture)
-        return 6;
-    
+    if (!heroTexture)   return EXIT_FAILURE;
     goldTexture = sfTexture_createFromFile(goldAddress, NULL);
-    if (!goldTexture)
-        return 7;
-    
+    if (!goldTexture)   return EXIT_FAILURE;
     doorTexture = sfTexture_createFromFile(doorAddress, NULL);
-    if (!doorTexture)
-        return 7;
-    
-    font = sfFont_createFromFile(textAddress);
-    if (!font)
-        return 8;
-    
+    if (!doorTexture)   return EXIT_FAILURE;
     goldIconTexture = sfTexture_createFromFile(goldIconAddress, NULL);
-    if (!goldIconTexture)
-        return 9;
-    
+    if(!goldIconTexture)return EXIT_FAILURE;
     stepsTexture = sfTexture_createFromFile(stepsAddress, NULL);
-    if (!stepsTexture)
-        return 10;
+    if (!stepsTexture)  return EXIT_FAILURE;
+  font = sfFont_createFromFile(textAddress);
+    if (!font)          return EXIT_FAILURE;
     
     for(i=0;i<12;i++)
     {
@@ -175,11 +158,9 @@ int main(int argc,char* argv[])
         sfSprite_setPosition(blockSprite[i+36], sscale);
     }
     
-
+    //argvCheck=0;
     
-    Point* way = (Point*) malloc(sizeof(Point) * 100);
-    
-    
+    if(argvCheck!=0){
     while (maxSum==-1 )
     {
         goldSpriteCount=0;
@@ -231,9 +212,66 @@ int main(int argc,char* argv[])
                 sfSprite_setPosition(grassSprite[count], sscale);
                 count++;
             }
-        
         findWay(0, 0, 0, way, 0);
     }
+    }
+    
+   if(argvCheck==1)
+        currentJSON();
+    if(argvCheck==0)
+        readJSON();
+    
+    if(argvCheck==0)
+    {
+        goldSpriteCount=0;
+        count=0;
+        
+        for(j=0;j<10;j++)
+            for(k=0;k<10;k++)
+            {
+                sscale.x=64+64*j;
+                sscale.y=64+64*k;
+            
+                grassSprite[count]=sfSprite_create();
+                
+                if(map[rtrnX(count)][rtrnY(count)]==-1)
+                sfSprite_setTexture(grassSprite[count],boxTexture, sfTrue);
+                
+                if(map[rtrnX(count)][rtrnY(count)]!=-1)
+                sfSprite_setTexture(grassSprite[count],grassTexture, sfTrue);
+                
+                if(map[rtrnX(count)][rtrnY(count)]!=0 && map[rtrnX(count)][rtrnY(count)]!=-1)
+                {
+                    if(goldSpriteCount<20)
+                    {
+                        goldSprite[goldSpriteCount]=sfSprite_create();
+                        sfSprite_setTexture(goldSprite[goldSpriteCount],goldTexture, sfTrue);
+                        sfSprite_setPosition(goldSprite[goldSpriteCount], sscale);
+                        
+                        goldIconText[goldSpriteCount] = sfText_create();
+                        textColor=sfBlack;
+                        
+                        goldScale.x=72+64*j;
+                        goldScale.y=94+64*k;
+                        sfText_setFont(goldIconText[goldSpriteCount], font);
+                        sfText_setCharacterSize(goldIconText[goldSpriteCount], 11);
+                        sfText_setColor(goldIconText[goldSpriteCount],textColor);
+                        sfText_move(goldIconText[goldSpriteCount], goldScale);
+                        gold=map[rtrnX(count)][rtrnY(count)];
+                        sprintf(goldArr, "%d",gold);
+                        sfText_setString(goldIconText[goldSpriteCount], goldArr);
+                        goldSpriteCount++;
+                    }
+                }
+
+                
+                sfSprite_setPosition(grassSprite[count], sscale);
+                count++;
+            }
+        findWay(0, 0, 0, way, 0);
+
+    }
+    
     for(j=0;j<10;j++)
     {
         for(k=0;k<10;k++)
@@ -244,7 +282,6 @@ int main(int argc,char* argv[])
         for(k=0;k<10;k++)
             stepMap[j][k]=0;
     
-    
     for(j=0;j<10;j++)
         for(k=0;k<10;k++)
         {
@@ -253,10 +290,9 @@ int main(int argc,char* argv[])
             stepsSprite[j][k] =sfSprite_create();
             sfSprite_setTexture(stepsSprite[j][k],stepsTexture, sfTrue);
             sfSprite_setPosition(stepsSprite[j][k], sscale);
-            
         }
-    //208x304
-  for(i=0;i<4;i++)
+    
+    for(i=0;i<4;i++)
     {  
     sscale.x=64;
     sscale.y=64;
@@ -266,15 +302,11 @@ int main(int argc,char* argv[])
     heroRect.width=52;
     heroRect.height=76;
     
-   
     heroSprite[i]=sfSprite_create();
     sfSprite_setTexture(heroSprite[i],heroTexture, sfTrue);
     sfSprite_setTextureRect(heroSprite[i], heroRect);
     sfSprite_setPosition(heroSprite[i], sscale);
-    
     }
-
-    
     for(i=4;i<8;i++)
     {
         sscale.x=64;
@@ -290,8 +322,6 @@ int main(int argc,char* argv[])
         sfSprite_setTextureRect(heroSprite[i], heroRect);
         sfSprite_setPosition(heroSprite[i], sscale);
     }
-    
-    
     
     
     sscale.x=640;
@@ -329,15 +359,11 @@ int main(int argc,char* argv[])
     sfText_move(fpsText, textScale);
     
     
-    
     sscale.x=560;
     sscale.y=10;
     goldIconSprite=sfSprite_create();
     sfSprite_setTexture(goldIconSprite,goldIconTexture, sfTrue);
     sfSprite_setPosition(goldIconSprite, sscale);
-    
-    
-    
     
     
     printf("%d\n", maxSum);
@@ -346,13 +372,8 @@ int main(int argc,char* argv[])
         printf("[%d]: %d %d    \n",i, maxWay[i].x, maxWay[i].y);
     
     printf("\n");
-    i=0;
-    
-    int fps = 0;
     sfClock_restart(fpsClock);
-    writeMap();
-    
-    ///////////////////////////////////////////////////////////////////////////
+
     while (sfRenderWindow_isOpen(window))
     {
         check=0;
@@ -363,7 +384,7 @@ int main(int argc,char* argv[])
         }
         char buff[15];
         fps++;
-        if (fmod(sfTime_asSeconds(sfClock_getElapsedTime(fpsClock)),0.25) < 0.001 && fps > 2)
+        if (fmod(sfTime_asSeconds(sfClock_getElapsedTime(fpsClock)),0.01) < 0.001 && fps > 2)
         {
             sprintf(buff, "FPS: %d", (int)(fps*(1/ sfTime_asSeconds(sfClock_getElapsedTime(fpsClock)))));
             sfText_setString(fpsText, buff);
@@ -376,21 +397,20 @@ int main(int argc,char* argv[])
         
         currentScale=sfSprite_getPosition(heroSprite[0]);
         
-        if(s==128*2)
+        if(s==64)
         {
             s=0;
             t++;
             currentGoldCount+=map[maxWay[t].x][maxWay[t].y];
-            
         }
         for(i=0;i<8;i++){
         if(maxSum!=-1)
         {
             if (maxWay[t+1].x==maxWay[t].x)
             {
-                if(currentScale.x<704)
+                if(currentScale.x<=704)
                 {
-                sscale.x=0.25;
+                sscale.x=speed;
                 sscale.y=0;
                 sfSprite_move(heroSprite[i], sscale);
                if(i==0)
@@ -399,10 +419,10 @@ int main(int argc,char* argv[])
             }
             else if (maxWay[t+1].y==maxWay[t].y)
             {
-                if(currentScale.y<704)
+                if(currentScale.y<=704)
                 {
                 sscale.x=0;
-                sscale.y=0.25;
+                sscale.y=speed;
                 sfSprite_move(heroSprite[i], sscale);
                     if(i==0)
                 s++;
@@ -410,15 +430,10 @@ int main(int argc,char* argv[])
                 }
             }
         }
-        
         }
-        
-        
-        
         
         sprintf(goldArr, "%d",currentGoldCount);
         sfText_setString(goldText, goldArr);
-        
         sfRenderWindow_clear(window, sfBlack);
         
         for (i=0;i<47;i++)
@@ -443,41 +458,38 @@ int main(int argc,char* argv[])
         forS=s;
         if(check==0)
         {
-            if(forS<=64 && forS>0)
+            if(forS<=16 && forS>0)
                 sfRenderWindow_drawSprite(window,heroSprite[0],NULL);
-            if(forS<=128 && forS>64)
+            if(forS<=32 && forS>16)
                 sfRenderWindow_drawSprite(window,heroSprite[1],NULL);
-            if(forS<=192 && forS>128)
+            if(forS<=48 && forS>32)
                 sfRenderWindow_drawSprite(window,heroSprite[2],NULL);
-            if(forS<=256 && forS>192)
+            if(forS<=64 && forS>48)
                 sfRenderWindow_drawSprite(window,heroSprite[3],NULL);
-
         }
         else if (check==1)
         {
-            if(forS<=64 && forS>0)
+            if(forS<=16 && forS>0)
                 sfRenderWindow_drawSprite(window,heroSprite[4],NULL);
-            if(forS<=128 && forS>64)
+            if(forS<=32 && forS>16)
                 sfRenderWindow_drawSprite(window,heroSprite[5],NULL);
-            if(forS<=192 && forS>128)
+            if(forS<=48 && forS>32)
                 sfRenderWindow_drawSprite(window,heroSprite[6],NULL);
-            if(forS<=256 && forS>192)
+            if(forS<=64 && forS>48)
                 sfRenderWindow_drawSprite(window,heroSprite[7],NULL);
         }
-        else
-            sfRenderWindow_drawSprite(window,heroSprite[0],NULL);
- if(currentScale.x==704 && currentScale.y==704)
+ if(currentScale.x==640 && currentScale.y==640)
         sfRenderWindow_drawSprite(window,heroSprite[0],NULL);
+        
         
         sfRenderWindow_drawText(window, text, NULL);
         sfRenderWindow_drawText(window, goldText, NULL);
         sfRenderWindow_drawText(window, fpsText, NULL);
         sfRenderWindow_drawSprite(window,goldIconSprite,NULL);
-        
+        Mdelay(10000);
         sfRenderWindow_display(window);
     }
    
-    
     sfTexture_destroy(blockTexture);
     sfTexture_destroy(grassTexture);
     sfTexture_destroy(goldTexture);
@@ -491,14 +503,12 @@ int main(int argc,char* argv[])
     
     sfRenderWindow_destroy(window);
     
-    return 0;
+    return EXIT_SUCCESS;
 }
-
 
 void findWay(int x, int y, int sum, Point* way, int counter)
 {
     int i = 0;
-    
     way[counter].x = x;
     way[counter].y = y;
     
@@ -507,12 +517,9 @@ void findWay(int x, int y, int sum, Point* way, int counter)
         if (sum > maxSum)
         {
             maxSum = sum;
-            
             maxWay = (Point*) malloc(sizeof(Point) * counter + 1);
-            
             for (i = 0; i < counter + 1; i++)
                 maxWay[i] = way[i];
-            
             sizeOfWay = counter + 1;
         }
     }
@@ -520,13 +527,10 @@ void findWay(int x, int y, int sum, Point* way, int counter)
     {
         if (x < 9 && map[x + 1][y] != -1)
             findWay(x + 1, y, sum + map[x + 1][y], way, counter + 1);
-        
         if (y < 9 && map[x][y + 1] != -1)
             findWay(x, y + 1, sum + map[x][y + 1], way, counter + 1);
     }
 }
-
-
 int rtrnX(int numb)
 {
     int tmp=numb;
@@ -541,8 +545,6 @@ int rtrnX(int numb)
         return tmp;
     }
 }
-
-
 int rtrnY(int numb)
 {
     if(numb<10)
@@ -550,16 +552,16 @@ int rtrnY(int numb)
     else
         return numb/10;
 }
-
 void Mdelay(unsigned int mseconds)
 {
     clock_t goal = mseconds + clock();
     while (goal > clock());
 }
-
 void currentJSON()
 {
     FILE* mapJson=fopen("/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/map.json", "w");
+    FILE* FakemapJson=fopen("/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/fakemap.json", "w");
+    
     int i,j;
     fprintf(mapJson,"[\n");
     for(i=0;i<10;i++)
@@ -568,37 +570,35 @@ void currentJSON()
         for(j=0;j<10;j++)
         {
             if(j!=9)
-            fprintf(mapJson, "[%2d], ",map[i][j]);
+                fprintf(mapJson, "[%2d], ",map[i][j]);
             else
                 fprintf(mapJson, "[%2d]",map[i][j]);
-
+            
         }
         fprintf(mapJson,"\n");
     }
     fprintf(mapJson,"]");
+    
+    
+    for(i=0;i<10;i++)
+        for(j=0;j<10;j++)
+            fprintf(FakemapJson,"%2d ",map[i][j]);
+    
+    
+    fclose(FakemapJson);
     fclose(mapJson);
     
 }
-
-void writeMap()
+void readJSON()
 {
     
-    FILE* mapJson=fopen("/Users/adimirbondar/Documents/lab/map.json", "w");
+    FILE* FakemapJson=fopen("/Users/adimirbondar/Documents/lab/lab6(csfml)/lab6(csfml)/fakemap.json", "r");
+    
     int i,j;
     for(i=0;i<10;i++)
         for(j=0;j<10;j++)
-                fprintf(mapJson, "%d ",map[i][j]);
-    fclose(mapJson);
-}
-
-void readMap()
-{
-    FILE* mapJson=fopen("/Users/adimirbondar/Documents/lab/map.json", "r");
-    int i,j;
-    for(i=0;i<10;i++)
-        for(j=0;j<10;j++)
-            fscanf(mapJson,"%d",&map[i][j]);
-    fclose(mapJson);
+            fscanf(FakemapJson,"%d ",&map[i][j]);
+    
+    fclose(FakemapJson);
     
 }
-
